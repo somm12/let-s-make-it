02a14ab3c21c0df2254da74e25c3b65aade93eec
“somm12” <sominan8744@gmail.com>
2023-03-04 13:07:10 +0900

    CORS done(connection with client port and server port)

--- client/src/Component/List.jsx
+++ client/src/Component/List.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const List = ({ list }) => {
-  console.log(list);
+  const [text, setText] = useState("");
+  useEffect(() => {
+    let body = {
+      a: "hello",
+    };
+    axios
+      .post("/api/test", body)
+      .then(function (res) {
+        setText(res.data.text);
+        console.log(res);
+      })
+      .catch(function (error) {
+        // 에러 핸들링
+        alert("요청 실패");
+        console.log(error);
+      });
+  }, []);
   return (
     <div>
+      {text}
       {list.map((li, idx) => (
         <h1 key={idx}>내용 : {li}</h1>
       ))}
