a4c147deae43fe6a30e7d3de3cf0ff0abe3f85d8
somm12 <sominan8744@gmail.com>
2023-12-15 20:43:46 +0900

    style: header style changed to background image

--- client/src/Component/Header/Header.jsx
+++ client/src/Component/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-
 import firebase from "../../firebase";
+import img1 from "../../img/1.jpg";
+import img2 from "../../img/2.jpg";
+import img3 from "../../img/3.jpg";
 
 import style from "./Header.module.scss";
 
@@ -16,53 +18,60 @@ const Heading = () => {
     firebase.auth().signOut();
     navigate("/");
   };
-
+  const images = [img1, img2, img3];
+  const chosenImage = images[Math.floor(Math.random() * images.length)];
+  console.log(chosenImage);
   return (
     <div className={style.headerWrapper}>
-      <div className={style.logoWrapper}>
-        <a className={style.serviceName} href="/">
-          Let's Make It!
-        </a>
-
-        <FontAwesomeIcon
-          onClick={() => {
-            setToggleBtn(!toggleBtn);
-          }}
-          icon={faBars}
-        />
+      <div className={style.headerBgImg}>
+        <img src={chosenImage} alt="" />
       </div>
-      {toggleBtn && (
-        <nav>
-          <header>
-            <div className={style.userNavbar}>
-              <Link className={style.mainMenu} to="/upload">
-                Upload
-              </Link>
-              <Link className={style.mainMenu} to="/">
-                Home
-              </Link>
+      <div className={style.navBarWrapper}>
+        <div className={style.logoWrapper}>
+          <a className={style.serviceName} href="/">
+            Let's Make It!
+          </a>
 
-              {user.accessToken ? (
-                <>
-                  <Link className={style.userMenu} to="/mypage">
-                    MyPage
-                  </Link>
-                  <Link className={style.userMenu} to="/bookmark">
-                    Bookmark
-                  </Link>
-                  <div className={style.userMenu} onClick={logOut}>
-                    LogOut
-                  </div>
-                </>
-              ) : (
-                <Link className={style.userMenu} to="/login">
-                  Login
+          <FontAwesomeIcon
+            onClick={() => {
+              setToggleBtn(!toggleBtn);
+            }}
+            icon={faBars}
+          />
+        </div>
+        {toggleBtn && (
+          <nav>
+            <header>
+              <div className={style.userNavbar}>
+                <Link className={style.mainMenu} to="/upload">
+                  Upload
                 </Link>
-              )}
-            </div>
-          </header>
-        </nav>
-      )}
+                <Link className={style.mainMenu} to="/">
+                  Home
+                </Link>
+
+                {user.accessToken ? (
+                  <>
+                    <Link className={style.userMenu} to="/mypage">
+                      MyPage
+                    </Link>
+                    <Link className={style.userMenu} to="/bookmark">
+                      Bookmark
+                    </Link>
+                    <div className={style.userMenu} onClick={logOut}>
+                      LogOut
+                    </div>
+                  </>
+                ) : (
+                  <Link className={style.userMenu} to="/login">
+                    Login
+                  </Link>
+                )}
+              </div>
+            </header>
+          </nav>
+        )}
+      </div>
     </div>
   );
 };
